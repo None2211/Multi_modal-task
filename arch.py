664fe8cc55c11309b7184fd8c1f98a2b92883fe7
none11 <83198357+None2211@users.noreply.github.com>
2024-03-07 01:50:53 +0900

    Update arch.py

--- arch.py
+++ arch.py
@@ -92,27 +92,9 @@ class ImageTextDecoderClassifier(nn.Module):
 
         return logits
 
-class channel_attention(nn.Module):
-    def __init__(self, in_planes, ratio=16):
-        super(channel_attention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-
-        self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
-        self.relu1 = nn.ReLU()
-        self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
-
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x0 = x
-        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
-        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
-        out = avg_out + max_out
-        return x0 * self.sigmoid(out)
-class DeepLabV3Plus(nn.Module):
+class multitask(nn.Module):
     def __init__(self):
-        super(DeepLabV3Plus, self).__init__()
+        super(multitask, self).__init__()
 
 
         self.backbone = models.resnet34(pretrained=True)
